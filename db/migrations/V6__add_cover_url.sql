cc4f2c58d51dd8ec90e806a53df8752d50f445b6
3nz0k <e.mrtnez7@gmail.com>
2026-07-28 13:24:36 +0200

    feat: add cover URLs and images for pieces

---
+++ db/migrations/V6__add_cover_url.sql
@@ -0,0 +1,10 @@
+INSERT INTO pieces (cover_url) VALUES
+('/uploads/covers/hallelujah.jpg'),
+('/uploads/covers/hier-encore.jpg'),
+('/uploads/covers/im-done.jpg'),
+('/uploads/covers/freestyle-du-sale.jpg'),
+('/uploads/covers/je-te-promets.jpg'),
+('/uploads/covers/la-boheme.jpg'),
+('/uploads/covers/les-champs-elysees.jpg'),
+('/uploads/covers/l-envie.jpg'),
+('/uploads/covers/j-irai-ou-tu-iras.jpg');
